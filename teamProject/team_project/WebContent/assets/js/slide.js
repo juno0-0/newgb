9a3c47c9ee4b45f2c69d2a6197f3fcc6720cdd14
Juno0-0 <darktraces@nate.com>
2021-04-05 15:13:30 +0900

    21/04/05
    teamproject front end

--- teamProject/team_project/WebContent/assets/js/slide.js
+++
@@ -1,7 +0,0 @@
-/**
- * 
- */
-//페이드인 페이드아웃
-		$('#slide3>ul>li').hide(); 
-		$('#slide3>ul>li:first-child').show(); 
-		setInterval(function(){ $('#slide3>ul>li:first-child').fadeOut().next().fadeIn().end(1000).appendTo('#slide3>ul'); },3000);
